b8418a59232281809a0eda0255e9797eeb45293b
weichieh515 <ace05152535@gmail.com>
2017-10-03 11:15:53 +0800

    Jenkins

--- public/client/src/app/card/card.js
+++ public/client/src/app/card/card.js
@@ -3,7 +3,6 @@
     $(document).ready(function () {
        rotateCard =  function(btn){
               var $card = $(btn).closest('.card-container');
-              console.log($card);
               if($card.hasClass('hover')){
                   $card.removeClass('hover');
               } else {
