8a7ad62c949cd7dfdd501dc09269bd9dcc6eca41
weichieh515 <ace05152535@gmail.com>
2017-10-11 10:18:53 +0800

    Jenkins

--- public/client/src/app/introduction/introduction.component.ts
+++ public/client/src/app/introduction/introduction.component.ts
@@ -1,7 +1,10 @@
 import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
-import './introduction.js';
+
+import { Member } from '../member';
+
 declare var $: any;
 
 @Component({
@@ -13,19 +16,25 @@ export class IntroductionComponent implements OnInit {
 
 
   section: any;
+  members: Member[];
+  faculty: Member[] = [];
+  staff: Member[] = [];
+
   private sub: any;
   private init = true;
-  constructor(private route: ActivatedRoute, private router: Router) {
+
+
+  constructor(private route: ActivatedRoute, private router: Router, private apiService: ApiService) {
 
 
     this.sub = this.route.params.subscribe(params => {
       if (params['section']) {
         this.section = params['section'];
         console.log('cons:' + this.section);
-        if(document.readyState === 'complete' && this.init == false){
+        if (document.readyState === 'complete' && this.init == false) {
           this.goSection(this.section);
         }
-        
+
       }
     });
 
@@ -34,12 +43,14 @@ export class IntroductionComponent implements OnInit {
 
   ngAfterViewInit() {
     if (this.section) {
-       this.goSection(this.section);
-       this.init = false;
+      this.goSection(this.section);
+      this.init = false;
     }
   }
 
   ngOnInit() {
+
+    this.getMembers();
 
     expandTabPanel();
     $(window).resize(function () {
@@ -78,4 +89,22 @@ export class IntroductionComponent implements OnInit {
     window.location.hash = section;
   }
 
+  getMembers(): void {
+    this.apiService
+      .getMembers()
+      .subscribe(data => {
+        this.members = data;
+        this.sort();
+      });
+  }
+
+  sort(): void {
+    for (var i = 0; i < this.members.length; i++) {
+      if (this.members[i].type == 'faculty') {
+        this.faculty.push(this.members[i]);
+      } else if (this.members[i].type == 'staff') {
+        this.staff.push(this.members[i]);
+      }
+    }
+  }
 }
